c8dd5378f93215ac9c7d39364ec079e2df3918b9
kl2karpenko <kl2.karpenko@gmail.com>
2023-08-20 12:47:52 +0200

    add export as a module

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-import typescript from "@rollup/plugin-typescript";
-import path from "path";
-import { typescriptPaths } from "rollup-plugin-typescript-paths";
+import typescript from '@rollup/plugin-typescript';
+import path from 'path';
+import { typescriptPaths } from 'rollup-plugin-typescript-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,23 +13,23 @@ export default defineConfig({
     minify: true,
     reportCompressedSize: true,
     lib: {
-      entry: path.resolve(__dirname, "src/main.tsx"),
-      fileName: "main",
-      formats: ["es", "cjs"],
+      entry: path.resolve(__dirname, 'src/main.tsx'),
+      fileName: 'main',
+      formats: ['es', 'cjs'],
     },
     rollupOptions: {
       external: [],
       plugins: [
         typescriptPaths({
           preserveExtensions: true,
-          tsConfigPath: 'tsconfig.json'
+          tsConfigPath: 'tsconfig.json',
         }),
         typescript({
           sourceMap: false,
           declaration: true,
-          outDir: "dist",
+          outDir: 'dist',
         }),
       ],
-    }
-  }
+    },
+  },
 });
